de6c27da68beaab2c46896e417c644de3051ff8e
jiangming <hong@qq.com>
2019-12-12 16:08:07 +0800

    第二次

--- public/js/article_release.js
+++ public/js/article_release.js
@@ -9,29 +9,57 @@ $.ajax({
     }
 });
 
-//为表单注册submit提交事件
+//为表单注册提交事件
 $("#articleBox").on("click",'#issue',function(){
     var formData = new FormData();
-    console.log($("#rich_content #tinymce p").html());
-    
+    // console.log($("#rich_content #tinymce p").html());
 	// 将选择到的文件追加到formData对象中
     formData.append('cover', $("#exampleInputFile")[0].files[0]);
     formData.append('title', $("#inputEmail3").val());
     formData.append('type',  $("#formBox").val());
     formData.append('date', $("#dateinput").val());
     formData.append('content', $("#rich_content").val());
+    formData.append('status',"已发布");
 
-    // // 获取管理员在表单中输入的
-    // $.ajax({
-    //     type:'post',
-    //     url:'http://localhost:8000/admin/article_publish',
-    //     data:formData,
-    //     processData:false,
-    //     contentType:false,
-    //     success:function(){
-    //         alert('ok');
-    //         // location.href="/admin/article_list.html"
-    //     }
-    // });
+    // 获取管理员在表单中输入的
+    $.ajax({
+        type:'post',
+        url:'http://localhost:8000/admin/article_publish',
+        data:formData,
+        processData:false,
+        contentType:false,
+        success:function(){
+            location.href="/admin/article_list.html"
+        }
+    });
+    return false
+});
+
+
+//为表单注册提交事件
+$("#articleBox").on("click",'#draft',function(){
+    var formData = new FormData();
+    // console.log($("#rich_content #tinymce p").html());
+    // 将选择到的文件追加到formData对象中
+    var trr=$("#rich_content_ifr").contents().find("body");
+    console.log(trr)
+    formData.append('cover', $("#exampleInputFile")[0].files[0]);
+    formData.append('title', $("#inputEmail3").val());
+    formData.append('type',  $("#formBox").val());
+    formData.append('date', $("#dateinput").val());
+    formData.append('content', trr.find("p").text());
+    formData.append('status',"草稿");
+
+    // 获取管理员在表单中输入的
+    $.ajax({
+        type:'post',
+        url:'http://localhost:8000/admin/article_publish',
+        data:formData,
+        processData:false,
+        contentType:false,
+        success:function(){
+            location.href="/admin/article_list.html"
+        }
+    });
     return false
 });
